78d34c3e5621c3ee40cdfc06d10f7e3455f80b47
Alex Beeken <aeb242@gmail.com>
2017-10-06 15:49:39 -0700

    initial commit

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,9 @@
 class VisitorsController < ApplicationController
+  before_action :authenticate_user!, only: :dashboard
+
+  def index
+    if current_user
+      redirect_to dashboard_path
+    end
+  end
 end
