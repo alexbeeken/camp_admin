f7f93fe835f1ced22e7e2509e35feb43ade7b9b4
Alex Beeken <aeb242@gmail.com>
2017-10-06 17:41:58 -0700

    simplify code but it still doesnt work the way its supposed to

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,3 @@
 class VisitorsController < ApplicationController
-  def dashboard
-    unless user_signed_in?
-      redirect_to index_path and return
-    end
-  end
+  before_action :authenticate_user!, only: :dashboard
 end
