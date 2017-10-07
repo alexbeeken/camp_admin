9f5ad3a64f60890dcb6a492c93266ffcfa2466c9
Alex Beeken <aeb242@gmail.com>
2017-10-06 18:54:03 -0700

    successfully submit application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,54 @@ class ApplicationsController < ActionController::Base
   def new
     @application = Application.new()
   end
+
+  def create
+    @application = Application.create!(secure_params.merge(user: current_user))
+    redirect_to root_path
+  end
+
+  private
+
+  def secure_params
+    params.require(:application).permit(
+       :first_name,
+       :last_name,
+       :email,
+       :address_1,
+       :address_2,
+       :address_city,
+       :address_state,
+       :address_zip,
+       :address_country,
+       :phone,
+       :gender,
+       :parents,
+       :birthdate,
+       :voice_part,
+       :lowest_note,
+       :highest_note,
+       :ear_learning,
+       :read_music,
+       :independent_voice,
+       :independent_learning,
+       :play_to_you,
+       :like_to_move,
+       :other_experience,
+       :author_experience,
+       :instrument_experience,
+       :dietary_preferences,
+       :dietary_restrictions,
+       :allergies,
+       :fitness_level,
+       :no_smoking_confirm,
+       :form_confirm,
+       :tent_confirm,
+       :returning_past_years,
+       :returning_improvement_ideas,
+       :new_why,
+       :new_attitude,
+       :new_inspiration,
+       :new_other_skills
+    )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006233910) do
+ActiveRecord::Schema.define(version: 20171007015154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20171006233910) do
     t.string "new_inspiration"
     t.string "new_other_skills"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
