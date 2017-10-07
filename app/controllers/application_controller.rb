65f24311173b2234eab4ad9314106a2f7271fe44
Alex Beeken <aeb242@gmail.com>
2017-10-06 17:24:04 -0700

    remove file and simplify paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(resource)
+    dashboard_path
+  end
 end
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,4 @@
 class VisitorsController < ApplicationController
-
   def dashboard
     unless user_signed_in?
       redirect_to index_path and return
